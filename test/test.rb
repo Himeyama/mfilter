0b66301d6c4258d2b61a3bdc56874c9c797ec69e
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2021-08-26 14:06:49 +0900

    create test script

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require "bundler/setup"
 require "mfilter"
+require "csv"
 
 t = Array.new(100){|i| -Math::PI + i * 2 * Math::PI / (100 - 1)}
 x = t.map{|e| Math::sin(e) + 0.25 * rand}
@@ -8,4 +9,9 @@ b = [0.2] * 5
 a = [1]
 
 y = MFilter::filter(b, a, x)
-p y
+
+open("test/data.dat", "w") do |f|
+    f.puts [t, x, y].transpose.map{|e| e.join(" ")}
+end
+
+# gnuplot -e 'plot "test/data.dat" using 1:2 w l title "Input Data", "test/data.dat" using 1:3 w l title "Filtered Data"; pause -1'
